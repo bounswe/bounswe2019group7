9e0499c383ee00fd3a8406823461ffa49a3c4668
Merve İlik <32928991+merveilik@users.noreply.github.com>
2019-05-01 14:14:09 +0300

    Add files via upload

--- practice-app/api_project/Event/views.py
+++ practice-app/api_project/Event/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from Event.models import Event 
+import eventcrawler from Event
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    
+
+    def list(self, request):
+        crawler = EventCrawler()
+        events = crawler.crawl()
+        data ={}
+        for i, event in enumerate(events):
+            data[i]= {
+                'event_text':event[0],
+                'event_date':event[1]
+    }
+    return render(request, 'events.html', data)
+
+    
+
+
+
