915d8f575bdb33831ee4c9b24610c714077ad00f
Merve İlik <32928991+merveilik@users.noreply.github.com>
2019-05-01 14:00:46 +0300

    Add files via upload

--- practice-app/api_project/Event/models.py
+++ practice-app/api_project/Event/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+	event_text = models.TextField(max_length=250)
+	event_date = models.DateTimeField()
+    
+	def __str__(self):
+		return self.event_text
