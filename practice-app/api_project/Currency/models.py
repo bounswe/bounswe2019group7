01a041d197658db38e72f68c5e38ee8c17285702
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2019-05-04 15:43:20 +0300

    Update models.py

--- practice-app/api_project/Currency/models.py
+++ practice-app/api_project/Currency/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 # TODO: API'dan veri cekilecek.
 class Currency(models.Model):
-    code = models.CharField(max_length=255)
-    value = models.FloatField()
+    from_currency = models.CharField(max_length=255)
+    to_currency = models.CharField(max_length=255)
+    exchange_rate = models.FloatField()
     id = models.IntegerField(primary_key=True)
 
     def __str__(self):
-        return str(self.code)+ " - "+str(self.value)
+        return str(self.from_currency)+ "/"+str(self.to_currency) + ": " + str(self.exchange_rate)
 
+
+# PEVAKXVH1TMR8SU9
