e85a67082b659df1da99b6b10fb0577bf67e9032
Unknown <anxealous@gmail.com>
2019-05-05 21:38:49 +0300

    Add events page

--- practice-app/frontend/src/routes.js
+++ practice-app/frontend/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const HomePage = React.lazy(() => import('./views/Homepage'));
 const Subscribe = React.lazy(() => import('./views/Subscribe'));
+const Events = React.lazy(() => import('./views/Events'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/homepage', name: 'HomePage', component: HomePage },
   { path: '/subscribe', name: 'Subscribe', component: Subscribe },
+  { path: '/events', name: 'Events', component: Events },
 ];
 
 export default routes;
