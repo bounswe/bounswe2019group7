ccaa01d361161416127aaa33c9f2adff1f7e6ef6
Unknown <anxealous@gmail.com>
2019-05-05 20:58:27 +0300

    Create Subscribe page

--- practice-app/frontend/src/routes.js
+++ practice-app/frontend/src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const HomePage = React.lazy(() => import('./views/Homepage'));
+const Subscribe = React.lazy(() => import('./views/Subscribe'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/homepage', name: 'HomePage', component: HomePage },
+  { path: '/subscribe', name: 'Subscribe', component: Subscribe },
 ];
 
 export default routes;
