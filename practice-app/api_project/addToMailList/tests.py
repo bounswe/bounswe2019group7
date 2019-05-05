6aa95fed8788d4c9898ae8693cf60968432d6ddb
Şafak <safakozdek@gmail.com>
2019-05-05 17:05:35 +0300

    Update tests.py
    Added tests for form validation

--- practice-app/api_project/addToMailList/tests.py
+++ practice-app/api_project/addToMailList/tests.py
@@ -7,3 +7,21 @@ class TestUrls(SimpleTestCase):
     def test_url_is_resolved(self):
         url = reverse('email-list-signup')
         self.assertEquals(resolve(url).func, email_list_signup)
+
+class TestForm1(SimpleTestCase):
+
+    def test_form_is_valid(self):
+        form = EmailSignupForm(data={
+            'email': 'asdasd@gmail.com'
+        })
+
+        self.assertTrue(form.is_valid())
+
+class TestForm2(SimpleTestCase):
+
+    def test_form_is_valid(self):
+        form = EmailSignupForm(data={
+            'randomName': 'asdasd@gmail.com'
+        })
+
+        self.assertFalse(form.is_valid())
