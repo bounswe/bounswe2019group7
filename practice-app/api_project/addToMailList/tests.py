df83c721d83659e6f62301b881e76e35683fed2f
Şafak <safakozdek@gmail.com>
2019-05-05 17:06:43 +0300

    Update tests.py
    Fixed form tests

--- practice-app/api_project/addToMailList/tests.py
+++ practice-app/api_project/addToMailList/tests.py
@@ -1,12 +1,15 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from addToMailList.views import email_list_signup
+from addToMailList.forms import EmailSignupForm
+
 
 class TestUrls(SimpleTestCase):
 
     def test_url_is_resolved(self):
         url = reverse('email-list-signup')
         self.assertEquals(resolve(url).func, email_list_signup)
+
 
 class TestForm1(SimpleTestCase):
 
