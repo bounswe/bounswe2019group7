b52a1ef8c2fab0427f490e8cdc6aa77a357480a5
Alperen Bağ <alperen.bag@boun.edu.tr>
2019-05-05 20:32:26 +0300

    Conversion added

--- practice-app/api_project/Currency/views.py
+++ practice-app/api_project/Currency/views.py
@@ -5,7 +5,9 @@ from .models import Currency
 from .serializers import CurrencySerializer
 import requests
 import json
+from rest_framework.decorators import action
 from django.http import JsonResponse
+from django.http import HttpResponseNotFound
 
 
 class CurrencyViewSet(viewsets.ModelViewSet):
@@ -16,17 +18,16 @@ class CurrencyViewSet(viewsets.ModelViewSet):
     serializer_class = CurrencySerializer
 
     def list(self, request):  # GET currencies/
-        currency_list = [("USD", "TRY"), ("BTC", "USD"), ("EUR", "TRY"), ("EUR", "USD"), ("USD", "CNY")]
+        currency_list = [("USD", "TRY"), ("TRY", "USD")]
         data_list = []
         for index, currency in enumerate(currency_list):
-            url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={" \
-                  "}&apikey=PEVAKXVH1TMR8SU9".format(
+            url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey=PEVAKXVH1TMR8SU9".format(
                     currency[0], currency[1])
 
             data_temp = requests.get(url)
             data = json.loads(data_temp.text)
-
             row = Currency.objects.filter(id=index)
+
             if len(row) == 1:
                 row.update(exchange_rate=data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
             else:
@@ -45,10 +46,14 @@ class CurrencyViewSet(viewsets.ModelViewSet):
         serializer = CurrencySerializer(currency)
         return Response(serializer.data)
 
-
-    """
-    def convertCurrency(self, money, Currency):
-        rate = Currency.exchange_rate
-        money *= rate
-        pass
-    """
+    @action(detail=True, methods=['get'])
+    def convert_currency(self, *args, **kwargs):    # GET currencies/convert/<from>/<to>/<amount>
+        try:
+            from_currency = str(kwargs['from_currency'])
+            to_currency = str(kwargs['to_currency'])
+            amount = float(kwargs['amount'])
+            row = Currency.objects.filter(from_currency=from_currency)
+            row = row.get(to_currency=to_currency)
+            return Response({"result":row.exchange_rate*amount})
+        except:
+            return HttpResponseNotFound("Invalid Currency")
