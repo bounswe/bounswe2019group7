b3982bde6addf2640cb5a7cfb5cbedd4d2a80a22
Şafak <safakozdek@gmail.com>
2019-05-05 15:41:50 +0300

    Add files via upload

---
+++ practice-app/api_project/mail/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from addToMailList.forms import EmailSignupForm
+from addToMailList.models import Signup
+# Create your views here.
+form = EmailSignupForm()
+
+def index(request):
+
+    if request.method == "POST":
+        email = request.POST["email"]
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'index.html', context)
+
+
