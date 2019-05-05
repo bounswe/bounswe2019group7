de4c026a52bd562fbf80c7ceb1b46161da660741
Şafak <safakozdek@gmail.com>
2019-05-05 16:34:39 +0300

    Update tests.py
    Added url tests

--- practice-app/api_project/addToMailList/tests.py
+++ practice-app/api_project/addToMailList/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from addToMailList.views import email_list_signup
 
-# Create your tests here.
+class TestUrls(SimpleTestCase):
+
+    def test_url_is_resolved(self):
+        url = reverse('email-list-signup')
+        self.assertEquals(resolve(url).func, email_list_signup)
