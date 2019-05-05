1196c51f4704e35f6b3d7b2f8f7724b7c7f0f122
Unknown <anxealous@gmail.com>
2019-05-06 02:03:28 +0300

    Redirect Subscribe Page

--- practice-app/frontend/src/views/Subscribe/Subscribe.js
+++ practice-app/frontend/src/views/Subscribe/Subscribe.js
@@ -30,7 +30,6 @@ import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
 
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 class Subscribe extends Component {
   constructor(props) {
@@ -42,12 +41,12 @@ class Subscribe extends Component {
     };
   }
 
-  baseUrl = "http://127.0.0.0:8000/";
+  baseUrl = "http://127.0.0.1:8000/";
 
-  subscribe = () => {
+  subscribe = (e) => {
+      e.preventDefault();
       console.log("Subscribing "+this.state.subMail);
 
-      axios.defaults.headers.common['X-CSRF-TOKEN'] = Cookies.get('csrftoken');
       axios.post(this.baseUrl+"subscribe/",{email: this.state.subMail})
       .then(()=>{
         alert("Thanks for subscribing!");
@@ -57,39 +56,17 @@ class Subscribe extends Component {
       })
   }
 
+  componentDidMount() {
+    window.open("localhost:8000/mail");
+  }
+
   handleChange = (e) => {
       this.setState({ [e.target.name] : e.target.value });
   }
 
   render() {
 
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col>
-            <Card>
-                <CardHeader>
-                    <i className="fa fa-envelope"></i><strong>Subscribe to Mail List</strong>
-                </CardHeader>
-                <CardBody>
-                    <Form>
-                       <Row form>
-                         <Col md="2">
-                            <Label>Email</Label>
-                         </Col>
-                         <Col md="8">
-                            <Input placeholder="abc@xyz.com" name="subMail" value={this.state.subMail} onChange={this.handleChange}/>
-                        </Col>
-                        <Col md="2">
-                            <Button onClick={this.subscribe} color="primary">Subscribe</Button>
-                         </Col>
-                        </Row>
-                    </Form>
-                </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+    return (<div/>
     );
   }
 }
