a052862e76153ec970ff8e28dd0c51486357da7b
unknown <skutaykilic@gmail.com>
2019-05-05 21:42:08 +0300

    Another update

---
+++ practice-app/frontend/src/views/Articles/Articles.js
@@ -0,0 +1,86 @@
+import React, { Component, lazy, Suspense } from 'react';
+import { Bar, Line } from 'react-chartjs-2';
+import {
+  Badge,
+  Button,
+  ButtonDropdown,
+  ButtonGroup,
+  ButtonToolbar,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Col,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Progress,
+  Row,
+  Table,
+  ListGroup,
+  ListGroupItem,
+  Form,
+  FormGroup,
+  FormText,
+  Input,
+  Label,
+} from 'reactstrap';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
+
+
+class Articles extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+      radioSelected: 2,
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  }
+
+  onRadioBtnClick(radioSelected) {
+    this.setState({
+      radioSelected: radioSelected,
+    });
+  }
+
+  render() {
+
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col sm="12" xl="12">
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i><strong>Articles</strong>
+              </CardHeader>
+              <CardBody>
+                <ListGroup>
+                  <ListGroupItem>Article1</ListGroupItem>
+                  <ListGroupItem>Article2</ListGroupItem>
+                  <ListGroupItem>Article3</ListGroupItem>
+                  <ListGroupItem>Article4</ListGroupItem>
+                </ListGroup>
+              </CardBody>
+            </Card>
+          </Col>
+
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default Articles;
