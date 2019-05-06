85a24c2ec363a4df8d147e91700dd17d290031e3
merve <merveili@hotmail.com>
2019-05-06 11:43:33 +0300

    update

--- practice-app/api_project/Event/eventcrawler.py
+++ practice-app/api_project/Event/eventcrawler.py
@@ -7,27 +7,25 @@ import unicodedata
 class Eventcrawler:
 	def __init__(self):
 		self.base_url = 'http://us.econoday.com/byday.asp?'
-		self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36'}
+		self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'+ 'Chrome/43.0.2357.132 Safari/537.36'}
 		self.information = []
 
 	def crawl(self):
-		currentdate=datetime.datetime.now()
+		current_date=datetime.datetime.now()
 
 		for i in range(4): #last 4 days
-			self.getNextDaysEvents(currentdate, self.information)
-			currentdate -= datetime.timedelta(days=1)
+			self.getNextDaysEvents(current_date, self.information)
+			current_date -= datetime.timedelta(days=1)
 
 		return self.information
 
-	def getNextDaysEvents(self, currentdate, information):
-		curr_url = self.base_url + 'day=' + str(currentdate.day) + \
-		           '&month=' + str(currentdate.month) + \
-		           '&year=' + str(currentdate.year)
+	def getNextDaysEvents(self, current_date, information):
+		curr_url = self.base_url + 'day=' + str(current_date.day) + \
+		           '&month=' + str(current_date.month) + \
+		           '&year=' + str(current_date.year)
 
 		req = requests.get(curr_url, headers=self.header)
-
 		soup = BeautifulSoup(req.text, 'html.parser')
-
 		evtDescRows = soup.find_all('tr', class_="dailyeventtext")
 
 		for row in evtDescRows:
@@ -45,6 +43,6 @@ class Eventcrawler:
 		        evtTime24H_CT = evtTime24H_ET - 1
 
 		        evtName = row.find_all('td')[2].find_all('a')[0].get_text()
-		        evtDT = str(datetime.datetime(currentdate.year, currentdate.month, currentdate.day, evtTime24H_CT, int(evtTime[3:5]), 0, 0))
+		        evtDT = str(datetime.datetime(current_date.year, current_date.month, current_date.day, evtTime24H_CT, int(evtTime[3:5]), 0, 0))
 		        information.append([evtName, evtDT])
 
--- practice-app/api_project/Event/views.py
+++ practice-app/api_project/Event/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from Event.models import Event 
 from Event.eventcrawler import Eventcrawler
 import json 
 
@@ -17,4 +15,3 @@ class EventViewSet(viewsets.ModelViewSet):
         for event in events:
             data.append({'event_text':event[0],'event_date':event[1]})
         return Response(json.dumps(data))
-          
