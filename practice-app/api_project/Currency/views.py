40377b8170728d48304524bbe899df94c1300f31
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2019-05-12 19:09:45 +0300

    Update views.py

--- practice-app/api_project/Currency/views.py
+++ practice-app/api_project/Currency/views.py
@@ -16,28 +16,6 @@ class CurrencyViewSet(viewsets.ModelViewSet):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
 
-    def list(self, request):  # GET currencies/
-        currency_list = [("USD", "TRY"), ("TRY", "USD")]
-        data_list = []
-        for index, currency in enumerate(currency_list):
-            url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey=PEVAKXVH1TMR8SU9".format(
-                    currency[0], currency[1])
-
-            data_temp = requests.get(url)
-            data = json.loads(data_temp.text)
-            row = Currency.objects.filter(id=index)
-
-            if len(row) == 1:
-                row.update(exchange_rate=data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
-            else:
-                Currency.objects.create(from_currency=data["Realtime Currency Exchange Rate"]['1. From_Currency Code'],
-                                        to_currency=data["Realtime Currency Exchange Rate"]["3. To_Currency Code"],
-                                        exchange_rate=data["Realtime Currency Exchange Rate"]["5. Exchange Rate"],
-                                        id=index)
-
-            data_list.append(data)
-
-        return Response(data_list)
 
     def retrieve(self, request, pk=None):  # GET articles/<id>/
         queryset = Currency.objects.all()
