c8ba891d47a1e1591a8d601e7fd853f2dd2e929d
merve <merveili@hotmail.com>
2019-05-12 12:02:23 +0300

    tests

--- practice-app/api_project/Event/tests.py
+++ practice-app/api_project/Event/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from .views import *
 
-# Create your tests here.
+
+class CurrencyTest(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+        self.client = Client()
+
+    def test_list(self):
+        response = self.client.get(reverse(EventViewSet.list))
+        self.assertEqual(response.status_code, 200)
+
+    
+
