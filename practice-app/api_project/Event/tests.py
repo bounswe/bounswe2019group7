c48ccc829c52cc1f450ec3f03684cbcb33b778db
nbmetin <berke.metin@boun.edu.tr>
2019-05-12 19:57:25 +0300

    Update tests.py

--- practice-app/api_project/Event/tests.py
+++ practice-app/api_project/Event/tests.py
@@ -1,5 +1,6 @@
+from django.test import SimpleTestCase
 from django.test import Client, TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
 from .views import *
 
 
@@ -13,5 +14,9 @@ class EventTest(TestCase):
         response = self.client.get(reverse(EventViewSet.list))
         self.assertEqual(response.status_code, 200)
 
-    
 
+class TestUrls(SimpleTestCase):
+
+    def test_list_url(self):
+        url = reverse('list')
+        self.assertEquals(resolve(url).func, list)
