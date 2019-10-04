43e10fd896c5aa33009be203562d678464936c36
Anil Can Kara <anil.cn.kr@gmail.com>
2019-10-05 00:16:55 +0300

    Template for api calls

--- TradersAppApiExample/app/src/main/java/com/example/anil/tradersappapiexample/MainActivity.kt
+++ TradersAppApiExample/app/src/main/java/com/example/anil/tradersappapiexample/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.anil.tradersappapiexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofitService = RetrofitInstance.getRetrofitInstance().create(ExampleApiInterface::class.java)
+        //retrofitService.getIsOdd().enqueue(Callback<ApiResponse>())
+
     }
 }
+
+class RetrofitInstance {
+
+    companion object {
+        val BASE_URL: String = "OUR API URL"
+        fun getRetrofitInstance(): Retrofit{
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+
+}
+interface ExampleApiInterface {
+    @GET("OUR API ENDPOINT")
+    fun getIsOdd(): retrofit2.Call<ApiResponse>
+}
+
+data class ApiResponse(
+    val isOdd : Boolean
+)
