8f544cdbad4b65e96264ef1f98102c27532b8064
Şafak <safakozdek@gmail.com>
2019-10-22 01:05:44 +0300

    Update main.js

--- frontend/CoreUI-template/src/main.js
+++ frontend/CoreUI-template/src/main.js
@@ -1,25 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'core-js/es6/promise'
-import 'core-js/es6/string'
-import 'core-js/es7/array'
+import "core-js/es6/promise";
+import "core-js/es6/string";
+import "core-js/es7/array";
 // import cssVars from 'css-vars-ponyfill'
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import App from "./App";
+import router from "./router";
+import axios from "axios";
 
+Vue.prototype.$http = axios.create({
+  baseURL: "http://100.26.202.213:8080/",
+  withCredentials: false
+});
 // todo
 // cssVars()
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  template: '<App/>',
+  template: "<App/>",
   components: {
     App
   }
-})
+});
