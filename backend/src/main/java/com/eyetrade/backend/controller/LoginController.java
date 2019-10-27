d333ac1023686e4bc6859f11dca392d0bd93b0fd
Kutay <skutaykilic@gmail.com>
2019-10-27 16:08:50 +0300

    Swagger documentation is prepared

--- backend/src/main/java/com/eyetrade/backend/controller/LoginController.java
+++ backend/src/main/java/com/eyetrade/backend/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.eyetrade.backend.controller;
 import com.eyetrade.backend.model.dto.LoginDto;
 import com.eyetrade.backend.service.LoginService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
-@Api(value = "Login")
+@Api(value = "Login", tags = {"Operations Related With Login"})
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
+    @ApiOperation(value = "Login with the username (email) and password", response = String.class)
     @PostMapping("")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
         String token = loginService.login(loginDto);
--- backend/src/main/java/com/eyetrade/backend/controller/RegistrationController.java
+++ backend/src/main/java/com/eyetrade/backend/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.eyetrade.backend.model.resource.UserResource;
 import com.eyetrade.backend.service.RegistrationService;
 import com.eyetrade.backend.mapper.UserMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +17,20 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = {"/registration"})
-@Api(value = "Registration")
+@Api(value = "Registration", tags = {"Operations Related With Registration"})
 public class RegistrationController {
 
     @Autowired
     private RegistrationService registrationService;
 
+    @ApiOperation(value = "Register a user with the needed information", response = UserResource.class)
     @PostMapping("/register")
-    public UserResource registerUser(@RequestBody @Valid UserDto userDto) {
-        return registrationService.save(UserMapper.dtoToEntity(userDto));
+    public ResponseEntity<UserResource> registerUser(@RequestBody @Valid UserDto userDto) {
+        UserResource user = registrationService.save(UserMapper.dtoToEntity(userDto));
+        return ResponseEntity.ok(user);
     }
 
+    @ApiOperation(value = "Confirm a registration by using the link from the user's confirmation mail", response = String.class)
     @GetMapping("/confirm-register")
     public ResponseEntity<String> confirmRegister(@RequestParam("token") String confirmationToken) {
         registrationService.confirmRegister(confirmationToken);
