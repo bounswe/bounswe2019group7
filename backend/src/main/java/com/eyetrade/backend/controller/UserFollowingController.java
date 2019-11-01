a53b313eb1b2890243d3da4af4206de2a5dcec27
Kutay <skutaykilic@gmail.com>
2019-11-01 21:13:28 +0300

    Retrieval of follower and following users was developed.

--- backend/src/main/java/com/eyetrade/backend/controller/UserFollowingController.java
+++ backend/src/main/java/com/eyetrade/backend/controller/UserFollowingController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Api(value = "User Following", tags = {"Operations Related With User following relations"})
@@ -28,11 +29,56 @@ public class UserFollowingController {
     @ApiOperation(value = "A user's request to follow another user", response = UserResource.class)
     @GetMapping("/follow")
     public ResponseEntity<UserResource> follow(
-            @RequestHeader("Authorization") String token, @RequestHeader("followingUserEmail") String followingUserEmail)
-    {
+            @RequestHeader("Authorization") String token,
+            @RequestHeader("followingUserEmail") String followingUserEmail
+    ) {
         UUID userId = jwtUserChecker.resolveBasicToken(token);
         UserResource followingUserResource = userFollowingService.followUser(userId, followingUserEmail);
         return ResponseEntity.ok(followingUserResource);
+    }
+
+
+    @ApiOperation(
+            value = "Get a user's followers. If an email is given we look for email owner's followers, " +
+                    "otherwise we look for the request sender's followers",
+            response = List.class
+    )
+    @GetMapping("/getFollowers")
+    public ResponseEntity<List<UserResource>> getFollowers(
+            @RequestHeader("Authorization") String token,
+            @RequestHeader(value = "otherUserEmail", required = false) String otherUserEmail
+    ) {
+        UUID userId = jwtUserChecker.resolveBasicToken(token);
+        List<UserResource> followers;
+        if(otherUserEmail == null){
+            followers = userFollowingService.getSelfFollowers(userId);
+        }
+        else{
+            followers = userFollowingService.getUserFollowers(otherUserEmail);
+        }
+        return ResponseEntity.ok(followers);
+    }
+
+
+    @ApiOperation(
+            value = "Get a user's followings. If an email is given we look for email owner's followings, " +
+                    "otherwise we look for the request sender's followings",
+            response = List.class
+    )
+    @GetMapping("/getFollowings")
+    public ResponseEntity<List<UserResource>> getFollowings(
+            @RequestHeader("Authorization") String token,
+            @RequestHeader(value = "otherUserEmail", required = false) String otherUserEmail
+    ) {
+        UUID userId = jwtUserChecker.resolveBasicToken(token);
+        List<UserResource> followings;
+        if(otherUserEmail == null){
+            followings = userFollowingService.getSelfFollowings(userId);
+        }
+        else{
+            followings = userFollowingService.getUserFollowings(otherUserEmail);
+        }
+        return ResponseEntity.ok(followings);
     }
 
 }
--- backend/src/main/java/com/eyetrade/backend/service/UserFollowingService.java
+++ backend/src/main/java/com/eyetrade/backend/service/UserFollowingService.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -36,6 +38,52 @@ public class UserFollowingService {
         userFollowingRepository.saveAndFlush(relation);
         // return the response
         return UserMapper.entityToResource(following);
+    }
+
+    // Get the followers of the current user
+    public List<UserResource> getSelfFollowers(UUID id){
+        User following = userRepository.findById(id);
+        return getFollowers(following);
+    }
+
+    // Get the followers of another user
+    public List<UserResource> getUserFollowers(String email){
+        User following = userRepository.findByEmail(email);
+        return getFollowers(following);
+    }
+
+    // Get the followings of the current user
+    public List<UserResource> getSelfFollowings(UUID id){
+        User follower = userRepository.findById(id);
+        return getFollowings(follower);
+    }
+
+    // Get the followings of another user
+    public List<UserResource> getUserFollowings(String email){
+        User follower = userRepository.findByEmail(email);
+        return getFollowings(follower);
+    }
+
+    private List<UserResource> getFollowers(User following){
+        // Our user will be the following but we need her followers
+        List<UserFollowing> relations = userFollowingRepository.findByFollowing(following);
+        List<UserResource> followers = new ArrayList<>();
+        for(UserFollowing relation : relations){
+            UserResource follower = UserMapper.entityToResource(relation.getFollower());
+            followers.add(follower);
+        }
+        return followers;
+    }
+
+    private List<UserResource> getFollowings(User follower){
+        // Our user will be the follower but we need her followings
+        List<UserFollowing> relations = userFollowingRepository.findByFollower(follower);
+        List<UserResource> followings = new ArrayList<>();
+        for(UserFollowing relation : relations){
+            UserResource following = UserMapper.entityToResource(relation.getFollowing());
+            followings.add(following);
+        }
+        return followings;
     }
 
 }
