1c3f68255b4c558fe2c2acc8ab01543fa6b3574d
memhak <mehmethakankurtoglu@gmail.com>
2019-11-02 20:51:24 +0300

    implement profile and update features

--- backend/src/main/java/com/eyetrade/backend/controller/UserProfileController.java
+++ backend/src/main/java/com/eyetrade/backend/controller/UserProfileController.java
@@ -1,11 +1,17 @@
 package com.eyetrade.backend.controller;
 
+import com.eyetrade.backend.mapper.UserMapper;
+import com.eyetrade.backend.model.dto.UserDto;
+import com.eyetrade.backend.model.entity.User;
 import com.eyetrade.backend.security.JwtUserChecker;
 import com.eyetrade.backend.service.UserProfileService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
 
 
 @Api(value = "User Profile", tags = {"User Profile operations"})
@@ -18,4 +24,24 @@ public class UserProfileController {
 
     @Autowired
     private JwtUserChecker jwtUserChecker;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(
+            @RequestHeader("Authorization") String token
+    ){
+        UUID userID = jwtUserChecker.resolveBasicToken(token);
+        User user = userProfileService.getUserProfile(userID);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/updateProfile")
+    public ResponseEntity<User> updateUserProfile(
+            @RequestHeader("Authorization") String token,
+            @RequestBody @Valid UserDto userDto
+    ){
+        UUID userID = jwtUserChecker.resolveBasicToken(token);
+        User user = userProfileService.updateProfile(userID, UserMapper.dtoToEntity(userDto));
+        return ResponseEntity.ok(user);
+    }
+
 }
