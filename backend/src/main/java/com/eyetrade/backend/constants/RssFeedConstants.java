60dd15621bbdf0d4bf5c9192ace85a55f4224d29
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2019-11-07 17:10:42 +0300

    Rss Constants are created

---
+++ backend/src/main/java/com/eyetrade/backend/constants/RssFeedConstants.java
@@ -0,0 +1,18 @@
+package com.eyetrade.backend.constants;
+
+/**
+ * Created by Emir Gökdemir
+ * on 7 Kas 2019
+ */
+
+public class RssFeedConstants {
+    static final String TITLE = "title";
+    static final String DESCRIPTION = "description";
+    static final String CHANNEL = "channel";
+    static final String LINK = "link";
+    static final String AUTHOR = "author";
+    static final String ITEM = "item";
+    static final String PUB_DATE = "pubDate";
+    static final String GUID = "guid";
+    static final String RSS_TURKEY_URL= "https://tradingeconomics.com/turkey/rss";
+}
