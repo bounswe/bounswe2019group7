e5f51487e547645ffe1c4b09dfd9a7684a37f4e0
Unknown <anxealous@gmail.com>
2019-11-11 21:04:35 +0300

    Get location and location name on tap

--- TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Activities/LocationPickerActivity.kt
+++ TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Activities/LocationPickerActivity.kt
@@ -2,16 +2,35 @@ package com.example.app.tradersapp
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_location_picker.*
+import java.util.*
+import android.app.Activity
+import android.content.Intent
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
 class LocationPickerActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private var loc:LatLng? = null
+    private var locName:String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +40,43 @@ class LocationPickerActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        pickLocationButton.setOnClickListener{
+            if(locName.isNullOrBlank()){
+                Toast.makeText(this, "Please select a valid location", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
+            val resultIntent = Intent()
+            resultIntent.putExtra("location", locName)
+            resultIntent.putExtra("locationX", loc?.longitude)
+            resultIntent.putExtra("locationY", loc?.latitude)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        mMap.setOnMapClickListener {
+            mMap.clear()
+            val gcd = Geocoder(this, Locale.ENGLISH)
+            val addresses = gcd.getFromLocation(it.latitude, it.longitude, 1)
+            if (addresses.size > 0) {
+                val name =
+                    if (addresses[0].adminArea != null)
+                        addresses[0].adminArea + ", " + addresses[0].countryName
+                    else addresses[0].countryName
+
+                loc = it
+                locName = name
+
+                if(!name.isNullOrBlank())
+                    mMap.addMarker(MarkerOptions().position(it).title(name)).showInfoWindow()
+            }
+        }
+
+
         if (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -41,8 +92,10 @@ class LocationPickerActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
         when (requestCode) {
             0 -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
