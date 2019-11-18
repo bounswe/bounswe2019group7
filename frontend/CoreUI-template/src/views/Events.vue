433b64b312d18c6036a23b27cbb72c5df7d44609
kemal-duru-2015400147 <32497010+kemal-duru-2015400147@users.noreply.github.com>
2019-11-19 02:13:58 +0300

    Add files via upload
    Events page is implemented with current endpoints.

--- frontend/CoreUI-template/src/views/Events.vue
+++ frontend/CoreUI-template/src/views/Events.vue
@@ -1,194 +1,89 @@
 <template>
-  <div class="animated fadeIn">
-    <b-row>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">USD</h3>
-            <h1 class="flag-icon flag-icon-um " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 01:30</h6>
-          <p style="float: right">IMF Meetings</p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">TRY</h3>
-            <h1 class="flag-icon flag-icon-tr " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 11:00</h6>
-          <p style="float: right"> Turkish government to unveil economic reforms</p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">JPY</h3>
-            <h1 class="flag-icon flag-icon-jp " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 01:30</h6>
-          <p style="float: right">Exports (YoY) (Sep) -8.2% </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">JPY</h3>
-            <h1 class="flag-icon flag-icon-jp " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 01:30</h6>
-          <p style="float: right"> Imports (YoY) (Sep) -11.9% </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">CNY</h3>
-            <h1 class="flag-icon flag-icon-cn " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 03:00</h6>
-          <p style="float: right">House Prices (YoY) (Sep) 8.8% </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">NZD</h3>
-            <h1 class="flag-icon flag-icon-nz " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 08:00</h6>
-          <p style="float: right"> Credit Card Spending (YoY) 6.0%</p>
-        </b-card>
-      </b-col>
-    </b-row>
-    <!--/.row-->
-    <b-row>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">INR</h3>
-            <h1 class="flag-icon flag-icon-in " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time All Day</h6>
-          <p style="float: right"> India - Assembly Elections in Maharashtra </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">EUR</h3>
-            <h1 class="flag-icon flag-icon-de " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time Tentative</h6>
-          <p style="float: right"> German Buba Monthly Report </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">BRL</h3>
-            <h1 class="flag-icon flag-icon-br " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 10:30</h6>
-          <p style="float: right"> Federal Tax Revenue 119.95B </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">KRW</h3>
-            <h1 class="flag-icon flag-icon-kr" style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 12:00</h6>
-          <p style="float: right"> PPI (YoY) (Sep) -0.6% </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">EUR</h3>
-            <h1 class="flag-icon flag-icon-es " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 13:00</h6>
-          <p style="float: right"> Spanish Trade Balance -2.49B </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">CAD</h3>
-            <h1 class="flag-icon flag-icon-ca" style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 19:00</h6>
-          <p style="float: right"> Core Retail Sales (MoM) (Aug) -0.1%</p>
-        </b-card>
-      </b-col>
-    </b-row>
-    <!--/.row-->
+    <div class="table">
+      <h1 align="center">Economic Events</h1>
+    <table>
+    <thead>
+      <tr>
+        <th>Time</th>
+        <th>Significance</th>
+        <th>Event</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr v-for="event in get_rows()">
+        <td>{{ event.stringDate}}</td>
+        <td>{{ event.score+"/5"}}</td>
+        <td>{{ event.title }}</td>
+    </tr>
+  </tbody>
+</table>
+<nav>
+  <b-pagination  align="center" :total-rows="this.events.length" v-model="currentPage" :per-page="elementsPerPage">
+  </b-pagination>
+</nav>
+</div>
+  </template>
+<script>
+export default {
+  name:"Events",
+  data() {
+    return {
+      isDisabled: false,
+      form: {
+        email: "",
+        password: ""
+      },
+      currentPage: 1,
+      elementsPerPage: 7,
+      events: []
+    };
+  },
+  methods: {
+    "get_rows": function get_rows() {
+      var start = (this.currentPage - 1) * this.elementsPerPage;
+      var end = start + this.elementsPerPage;
+      return this.events.slice(start, end);
+    },
+  },
+  created() {
+    this.events = [];
+    this.$http.get('/event/get_events')
+      .then((response) => {
+        if(response.status==200){
+        this.events = response.data.instances;
+      }
+      });
+  }
+};
+</script>
+<style>
+table {
+  font-family: 'Arial';
+  width: 750px;
+  border-collapse: collapse;
+  border: 3px solid #44475C;
+  margin-left: auto;
+  margin-right: auto;
+}
+table th {
+  text-align: center;
+  background: #30343D;
+  color: #FFF;
+  padding: 8px;
+  min-width: 30px;
+}
+table td {
+  text-align: center;
+  padding: 8px;
+  border-right: 2px solid #44475C;
+}
+.pagination {
+  font-family: 'Arial', sans-serif;
+  text-align: right;
+  width: 750px;
+  padding: 8px;
+  margin-left: auto;
+  margin-right: auto;
+}
 
-    <b-row>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">ZAR</h3>
-            <h1 class="flag-icon flag-icon-za " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 20.30</h6>
-          <p style="float: right"> Core CPI (YoY) (Sep) 4.3% </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">TRY</h3>
-            <h1 class="flag-icon flag-icon-tr " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 19:50</h6>
-          <p style="float: right">Auto Production YoY SEP 10.7% </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">TRY</h3>
-            <h1 class="flag-icon flag-icon-tr " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 05:30</h6>
-          <p style="float: right"> Central Government Debt SEP TRY1.248T </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">TRY</h3>
-            <h1 class="flag-icon flag-icon-tr " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 10:00</h6>
-          <p style="float: right">Tourism Revenues Q3 $7.97B </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">TRY</h3>
-            <h1 class="flag-icon flag-icon-tr " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 10:00</h6>
-          <p style="float: right"> Unemployment Rate AUG 13.9% </p>
-        </b-card>
-      </b-col>
-      <b-col sm="6" md="4">
-        <b-card>
-          <div slot="header" style="clear: both">
-            <h3 style="float: left">GBP</h3>
-            <h1 class="flag-icon flag-icon-gb " style="float: right"></h1>
-          </div>
-          <h6 style="float: left">Time 19:00</h6>
-          <p style="float: right">Rightmove House Price Index (MoM) -0.2% </p>
-        </b-card>
-      </b-col>
-    </b-row>
-    <!--/.row-->
-  </div>
-</template>
+</style>
