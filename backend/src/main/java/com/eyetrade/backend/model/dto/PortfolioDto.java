563a5893d304b9c25c5fd2805f9cf5b6308fc107
memhak <mehmethakankurtoglu@gmail.com>
2019-11-20 14:29:34 +0300

    implement portfolio entity

---
+++ backend/src/main/java/com/eyetrade/backend/model/dto/PortfolioDto.java
@@ -0,0 +1,14 @@
+package com.eyetrade.backend.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PortfolioDto {
+    public String name;
+    public String ownerID;
+    public String currencies;
+}
