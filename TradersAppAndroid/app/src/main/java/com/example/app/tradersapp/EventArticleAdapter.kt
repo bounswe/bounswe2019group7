a1c6128065c45ae8e21d79348d68c7ff633629de
Anil Can Kara <anil.cn.kr@gmail.com>
2019-11-22 19:45:44 +0300

    Fetch articles from db, add to recycler view an create article detail fragment

--- TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/EventArticleAdapter.kt
+++ TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/EventArticleAdapter.kt
@@ -1,15 +1,22 @@
 package com.example.app.tradersapp
 
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.example.app.tradersapp.Fragments.ArticleDetailFragment
 import com.example.app.tradersapp.Fragments.EventArticleHolder
 import com.example.app.tradersapp.Fragments.EventArticleModel
+import com.example.app.tradersapp.RegistrationActivity.RegisterCallback.activity
 
 
-class EventArticleAdapter(private val list: List<EventArticleModel>)
+class EventArticleAdapter(private val list: List<EventArticleModel>, context: Context)
         : RecyclerView.Adapter<EventArticleHolder>() {
 
+        val mContext = context
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventArticleHolder {
             val inflater = LayoutInflater.from(parent.context)
             return EventArticleHolder(inflater, parent)
@@ -18,6 +25,24 @@ class EventArticleAdapter(private val list: List<EventArticleModel>)
         override fun onBindViewHolder(holder: EventArticleHolder, position: Int) {
             val eaModel: EventArticleModel = list[position]
             holder.bind(eaModel)
+            var fragment: Fragment? = null
+            holder.itemView.setOnClickListener {
+                // TODO: Open the article
+                val articleDetailFragment = ArticleDetailFragment()
+                val bundle = Bundle()
+                bundle.apply{
+                    putInt("image", holder.eaImage!!.tag as Int)
+                    putString("title", holder.eaTitle!!.text.toString())
+                    putString("body", holder.eaBody!!.text.toString())
+                }
+                articleDetailFragment.arguments = bundle
+                val transaction = (mContext as FragmentActivity).supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.nav_host_fragment, articleDetailFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+
+
+            }
         }
 
         override fun getItemCount(): Int = list.size
--- TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Fragments/EventArticleHolder.kt
+++ TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Fragments/EventArticleHolder.kt
@@ -16,19 +16,33 @@ class EventArticleHolder(inflater: LayoutInflater, parent:ViewGroup) : RecyclerV
     var eaImage: ImageView? = null
     var eaTitle: TextView? = null
     var eaBody: TextView? = null
+    var articleAuthorName: TextView? = null
+    var articleScore: TextView? = null
+    var articleChangeDate: TextView? = null
     init {
-
         eaImage = itemView.findViewById(R.id.eaImage)
         eaTitle = itemView.findViewById(R.id.eaTitle)
         eaBody = itemView.findViewById(R.id.eaBody)
+        articleAuthorName = itemView.findViewById(R.id.articleAuthorName)
     }
 
     fun bind(item: EventArticleModel) {
         eaBody?.text = item.body
         eaTitle?.text = item.title
         eaImage?.setImageResource(item.image)
+        eaImage?.tag = item.image
+        articleAuthorName?.text = "Author: \n" + item.authorName + " " + item.authorSurname
     }
 
 }
 
-data class EventArticleModel(val image: Int, val title: String, val body: String)
+data class EventArticleModel(
+     val image: Int,
+     val title: String,
+     val body: String,
+     val authorName: String?,
+     val authorSurname: String?,
+     val score: Double?,
+     val changeDate: String?
+
+)
