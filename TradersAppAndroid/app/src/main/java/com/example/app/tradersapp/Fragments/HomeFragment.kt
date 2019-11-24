69a46dea99dbfeed33413d0cb9a0720340d71aa3
Unknown <anxealous@gmail.com>
2019-11-24 19:10:57 +0300

    Change from java to kotlin

---
+++ TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Fragments/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.example.app.tradersapp.Fragments;
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v4.app.Fragment
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.app.tradersapp.*
+import kotlinx.android.synthetic.main.fragmen_home.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+public class HomeFragment extends Fragment {
+
+    private val retrofitService = RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
