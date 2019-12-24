92ac2b45c2f9f32e345bf946a966163b00453a95
Unknown <anxealous@gmail.com>
2019-12-24 18:32:05 +0300

    Withdraw and exchange

--- TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Fragments/ExchangeFundsFragment.kt
+++ TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Fragments/ExchangeFundsFragment.kt
@@ -5,20 +5,23 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.app.tradersapp.ApiInterface
-import com.example.app.tradersapp.EyeTradeUtils
+import com.example.app.tradersapp.*
+import kotlinx.android.synthetic.main.fragment_exchange_funds.*
 
-import com.example.app.tradersapp.R
-import com.example.app.tradersapp.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ExchangeFundsFragment : Fragment() {
 
     private var sp: SharedPreferences? = null
     private val retrofitService =
         RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,9 +33,34 @@ class ExchangeFundsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        EyeTradeUtils.showSpinner(activity)
 
         sp = PreferenceManager.getDefaultSharedPreferences(context)
+
+        exchangeFundsSubmit.setOnClickListener {
+
+            val fundAmount = exchangeFundAmount1.text.toString().toDouble()
+            val currency1 = exchangeFundTypeSpinner1.selectedItem.toString()
+            val currency2 = exchangeFundTypeSpinner2.selectedItem.toString()
+
+            retrofitService.exchangeTransaction(
+                sp?.getString("token", null),
+                ExchangeTransactionInformation(fundAmount, currency1, currency2)
+            )
+                .enqueue(object : Callback<ExchangeTransactionResponse> {
+                    override fun onFailure(call: Call<ExchangeTransactionResponse>, t: Throwable) {
+                        EyeTradeUtils.toastErrorMessage(context!!, t)
+                    }
+
+                    override fun onResponse(
+                        call: Call<ExchangeTransactionResponse>,
+                        response: Response<ExchangeTransactionResponse>
+                    ) {
+                        (context as FragmentActivity).supportFragmentManager.popBackStack()
+                    }
+                })
+
+        }
+
     }
 
 }
--- TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Fragments/WithdrawFundsFragment.kt
+++ TradersAppAndroid/app/src/main/java/com/example/app/tradersapp/Fragments/WithdrawFundsFragment.kt
@@ -5,21 +5,22 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.app.tradersapp.ApiInterface
-import com.example.app.tradersapp.EyeTradeUtils
+import com.example.app.tradersapp.*
+import kotlinx.android.synthetic.main.fragment_withdraw_funds.*
 
-import com.example.app.tradersapp.R
-import com.example.app.tradersapp.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class WithdrawFundsFragment : Fragment() {
-
     private var sp: SharedPreferences? = null
     private val retrofitService =
         RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
-    
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,9 +31,33 @@ class WithdrawFundsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        EyeTradeUtils.showSpinner(activity)
 
         sp = PreferenceManager.getDefaultSharedPreferences(context)
+
+        withdrawFundsSubmit.setOnClickListener {
+
+            val fundAmount = withdrawFundAmount.text.toString().toDouble()
+            val currency = withdrawFundTypeSpinner.selectedItem.toString()
+
+            retrofitService.sellTransaction(
+                sp?.getString("token", null),
+                SellTransactionInformation(fundAmount, currency)
+            )
+                .enqueue(object : Callback<SellTransactionResponse> {
+                    override fun onFailure(call: Call<SellTransactionResponse>, t: Throwable) {
+                        EyeTradeUtils.toastErrorMessage(context!!, t)
+                    }
+
+                    override fun onResponse(
+                        call: Call<SellTransactionResponse>,
+                        response: Response<SellTransactionResponse>
+                    ) {
+                        (context as FragmentActivity).supportFragmentManager.popBackStack()
+                    }
+                })
+
+        }
+
     }
 
 }
